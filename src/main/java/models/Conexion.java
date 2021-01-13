29c2ad7d0e4985d5a55f1feeda1dfd4411145d74
FoNaK1999 <57738140+FoNaK1999@users.noreply.github.com>
2021-01-12 22:10:58 -0300

    Update Conexion.java

--- src/main/java/models/Conexion.java
+++ src/main/java/models/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
     private String port="3306";
     private String host="localhost";
     private String database="bakerynico";
-    private String classname="com.mysql.jdbc.Driver";
+    private String classname="com.mysql.cj.jdbc.Driver";
     private String url="jdbc:mysql://node61115-env-7271523.jelastic.saveincloud.net/"+database;
     Connection con;
     
